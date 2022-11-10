01471d8a2bf956cae6982260e97eeb2574ff6c29
Leonardo Betetto <60801645+lmbbetetto@users.noreply.github.com>
2022-11-10 12:16:48 -0300

    commit

--- src/components/apoie/apoie.tsx
+++ src/components/apoie/apoie.tsx
@@ -1,11 +1,33 @@
 import "./styles.css"
 
+import { BsCashCoin } from 'react-icons/bs'
+import { BsCreditCardFill } from 'react-icons/bs'
+import { RiHandHeartFill } from 'react-icons/ri'
+
 export function Apoie() {
     return (
         <>
             <div className="trans_header">
-                <h1 className="nome_trans">Descubra como nos ajudar</h1>
+                <h1 className="nome_trans">Apoie</h1>
                 <h1 className="trans_linha"></h1>
+            </div>
+
+            <div className="container_apoie">
+                <div className="block">
+                    <p className="icon_apoie"><BsCashCoin /></p>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.</p>
+                </div>
+
+                <div className="block">
+                    <p className="icon_apoie"><BsCreditCardFill /></p>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.</p>
+                </div>
+
+                <div className="block">
+                    <p className="icon_apoie"><RiHandHeartFill /></p>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.</p>
+                </div>
+
             </div>
         </>
     )
