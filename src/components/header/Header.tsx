c11833d0e40dd49d661585830d48b0de6e6a0b39
Leonardo Betetto <60801645+lmbbetetto@users.noreply.github.com>
2022-12-06 22:20:47 -0300

    commit header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -13,38 +13,75 @@ function Header() {
   const [active, setActive] = useState(false)
 
   return (
-    <div className='main-container'>
-      <header className='mobile'>
-        <div className='hamburguer-container'>
-          <button
-            className='hamburguer'
-            onClick={() => {
-              setActive(!active)
-            }}
-          >
-            <AnimatedHumburguerButton active={active} />
-          </button>
-        </div>
-        <div className={active ? "activeSidenav" : "sidenav"}>
-          <div className="container_mobile container-header">
-            <div className="mobile_1">
-              <Link to="/"><a>Início</a></Link>
-              <Link to="/sobre"><a>Sobre</a></Link>
-              <Link to="/transparencia"><a>Transparência</a></Link>
-              <div className='linha'>
-                <Link to="/contato">Contato</Link>
-              </div>
-              <div className='socials'>
-                <a href="https://www.facebook.com/lar.dosidosos.12" target="_blank"><BsFacebook /></a>
-                <a href="https://www.instagram.com/lardosidososppta/" target="_blank"><BsInstagram /></a>
+    <>
+      <div className='main-container'>
+        <header className='mobile'>
+          <div className='hamburguer-container'>
+            <button
+              className='hamburguer'
+              onClick={() => {
+                setActive(!active)
+              }}
+            >
+              <AnimatedHumburguerButton active={active} />
+            </button>
+          </div>
+          <div className={active ? "activeSidenav" : "sidenav"}>
+            <div className="container_mobile container-header">
+              <div className="mobile_1">
+                <Link to="/"><a>Início</a></Link>
+                <Link to="/sobre"><a>Sobre</a></Link>
+                <Link to="/transparencia"><a>Transparência</a></Link>
+                <div className='linha'>
+                  <Link to="/contato">Contato</Link>
+                </div>
+                <div className='socials'>
+                  <a href="https://www.facebook.com/lar.dosidosos.12" target="_blank"><BsFacebook /></a>
+                  <a href="https://www.instagram.com/lardosidososppta/" target="_blank"><BsInstagram /></a>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <Link to="/"><img src={logo} alt="Lar dos Idosos" className="logo__casa" /></Link>
-        <Link to="/apoie"><button className='btn_doe1'>Apoie</button></Link>
-      </header>
-    </div>
+          <Link to="/"><img src={logo} alt="Lar dos Idosos" className="logo__casa" /></Link>
+          <Link to="/apoie"><button className='btn_doe1'>Apoie</button></Link>
+        </header>
+      </div>
+
+      <div className='main_mobile'>
+        <header className='mobile_mobile'>
+          <div className='hamburguer-container'>
+            <button
+              className='hamburguer'
+              onClick={() => {
+                setActive(!active)
+              }}
+            >
+              <AnimatedHumburguerButton active={active} />
+            </button>
+          </div>
+          <div className={active ? "activeSidenav" : "sidenav"}>
+            <div className="container_mobile container-header">
+              <div className="mobile_1">
+                <Link to="/"><a>Início</a></Link>
+                <Link to="/sobre"><a>Sobre</a></Link>
+                <Link to="/transparencia"><a>Transparência</a></Link>
+                <div className='linha'>
+                  <Link to="/contato">Contato</Link>
+                </div>
+                <div className='socials'>
+                  <a href="https://www.facebook.com/lar.dosidosos.12" target="_blank"><BsFacebook /></a>
+                  <a href="https://www.instagram.com/lardosidososppta/" target="_blank"><BsInstagram /></a>
+                </div>
+                <Link to="/apoie"><button className='btn_doe2'>Apoie</button></Link>
+              </div>
+            </div>
+          </div>
+          <div>
+            <Link to="/"><img src={logo} alt="Lar dos Idosos" className="logo__casa" /></Link>
+          </div>
+        </header>
+      </div>
+    </>
   )
 }
 
--- src/homeStyles.tsx
+++ src/homeStyles.tsx
@@ -2,6 +2,10 @@ import styled from "styled-components";
 
 export const Container = styled.div`
 
+    .mobile_mobile {
+      display: none;
+    }
+
     .main-container {
       position: fixed;
       width: 100%;
@@ -58,4 +62,80 @@ export const Container = styled.div`
       max-width: 60px;
     }
   }
+
+  @media (max-width: 600px) {
+    .main-container {
+      display: none;
+    }
+
+    .main_mobile {
+      position: fixed;
+      width: 100%;
+      height: 75px;
+      display: flex;
+
+      background-color: var(--color-white);
+      box-shadow: 5px 5px 30px rgba(0,0,0,0.5);
+    }
+
+    .mobile_mobile {
+      width: 80%;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      align-items: center;
+      padding: 1rem;
+
+    }
+
+    .logo__casa {
+      max-width: 50px;
+      /* margin-left: 10rem; */
+    }
+
+      .sidenav {
+        position: fixed;
+        width: 100%;
+        height: 100vh;
+        background: #fff;
+        transform: translateX(-110%);
+        transition: all 0.5s ease-in-out;
+        z-index: 1;
+      }
+
+      .activeSidenav {
+        position: fixed;
+        width: 100%;
+        height: 100vh;
+        background: #fff;
+        transform: translateX(-50%);
+        transition: all 0.5s ease-in-out;
+        z-index: 1;
+
+      .hamburguer {
+        background: transparent;
+      }
+
+      .btn_doe2 {
+        margin-top: 2rem;
+        width: 110px;
+        display: inline-block;
+        background: var(--color-red);
+        color: var(--color-white);
+        padding: 0.75rem 1.2rem;
+        right: 0.05rem;
+        bottom: 1.1rem;
+        border-radius: 1rem;
+        cursor: pointer;
+        letter-spacing: 1.5px;
+
+        transition: var(--transition);
+      }
+
+      .btn_doe2:hover {
+        background: var(--color-green-primary);
+        color: var(--color-white);
+        border-color: transparent;
+      }
+    }
 `
